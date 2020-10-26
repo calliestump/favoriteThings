0d25c9fad6d1b5d3f20ca2d4bf33a90783f6e6ec
Callie Stump <callie@stu.mp>
2020-10-26 14:59:50 -0400

    adds elements to a new list using push() funciton
    Co-authored_by: Nick Hennessy <njhnny@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function() {
   $("form#favoriteThings").submit(function() {
-    let array = [];
+    const array = [];
     array.push($("#favFood").val());
     array.push($("#favSeason").val());
     array.push($("#favAnimal").val());
     array.push($("#favSport").val());
-    alert(array)
+
+    let modifiedArray = [];
+    modifiedArray.push(array[1]);
+    modifiedArray.push(array[0]);
+    modifiedArray.push(array[2]);
+    alert(modifiedArray);
+
+    event.preventDefault();
   });
 });
 
+/* let modifiedArray = array[1]
+modifiedArray.push(array.slice(0,3)); */
